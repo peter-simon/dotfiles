c5e0f2e703ecb511efe6da891c2dc2d9feab75fe
Peter Simon <simon.peter.90@gmail.com>
2020-04-18 17:23:59 +0200

    next

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# clone and install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --key-bindings --completion --update-rc
+
+sudo apt-get install silversearcher-ag
+# create the symlinks from the dotfile repo pointing to their existing location
+
+# extend aliases
+ln -sv ~/.dotfiles/runcom/.bash_profile ~/.bashrc
+# apply env variables
+source .env
