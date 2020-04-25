cbc82d9960de078371feae169f00b29baacf7587
Peter Simon <simon.peter.90@gmail.com>
2020-04-25 18:00:36 +0200

    Git config added, and linkage done to user home

--- install.sh
+++ install.sh
@@ -11,3 +11,8 @@ sudo apt-get colordiff
 # create the symlinks from the dotfile repo pointing to their existing location
 ln -sv ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sv ~/dotfiles/.inputrc ~/.inputrc
+if [ -f ~/.gitconfig ]; then
+	cp ~/.gitconfig ~/.gitconfig.backup
+	echo "Overwriting .gitconfig. Backup created."
+fi
+ln -svf ~/dotfiles/git/.gitconfig ~/.gitconfig
