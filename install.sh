7fae71b17a8f5128e47c318b22f5cc7c1bdae259
Peter Simon <simon.peter.90@gmail.com>
2020-05-03 17:22:38 +0200

    Install fd

--- install.sh
+++ install.sh
@@ -4,13 +4,19 @@
 # clone and install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --update-rc
+
 # Install ripgrep
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
+
 # install vim plugin manger
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo apt-get colordiff
+
+# install fd
+curl -LO https://github.com/sharkdp/fd/releases/download/v8.0.0/fd_8.0.0_amd64.deb
+sudo dpkg -i fd_8.0.0_amd64.deb
 
 # create the symlinks from the dotfile repo pointing to their existing location
 ln -sv ~/dotfiles/.bash_profile ~/.bash_profile
